d39953cde9f3d6c9776a89cca3bf050765051299
Ivan Montiel <idmontie@asu.edu>
2015-11-18 16:40:31 -0700

    Use packages for validation and humanizing

--- mobile-src/lib/utilities/humanizer.js
+++ mobile-src/lib/utilities/humanizer.js
@@ -1,28 +1,25 @@
-// TODO turn this into a separate package.
-// meteor add momentjs:moment
-Humanizer = {
-  Date: {
-    humanize: function(text, inFormat, outFormat) {
-      if (text === '') return '';
+Humanize.Date = {
+  humanize: function(text, inFormat, outFormat) {
+    if (text === '') return '';
 
-      inFormat  = inFormat || "MM-DD-YYYY";
-      outFormat = outFormat || "MMMM D, YYYY";
-      return moment(text, inFormat).format(outFormat);
-    },
+    inFormat  = inFormat || "MM-DD-YYYY";
+    outFormat = outFormat || "MMMM D, YYYY";
+    return moment(text, inFormat).format(outFormat);
   },
-  Time: {
-    humanize: function(text, inFormat, outFormat) {
-      if (text === '') return '';
+};
 
-      inFormat  = inFormat || "HH:mm";
-      outFormat = outFormat || "h:mm A";
-      return moment(text, inFormat).format(outFormat);
-    }
+Humanize.Time = {
+  humanize: function(text, inFormat, outFormat) {
+    if (text === '') return '';
+
+    inFormat  = inFormat || "HH:mm";
+    outFormat = outFormat || "h:mm A";
+    return moment(text, inFormat).format(outFormat);
   }
 };
 
 // Alias
 
 if ( typeof __ === "undefined" ) {
-  __ = Humanizer;
+  __ = Humanize;
 }
---
+++ mobile-src/server/README.md
@@ -0,0 +1,3 @@
+NO SERVER FILES!
+
+This application will not have a server component
